dd0003a9e7bfcf97e68543d11be4951d95cba701
viniciusbds <vinicius.barbosa.silva@ccc.ufcg.edu.br>
2022-07-27 01:18:17 -0300

    Add consumprion DB operations

--- storage/consumption.go
+++ storage/consumption.go
@@ -1,1 +1,17 @@
 package storage
+
+import uuid "github.com/satori/go.uuid"
+
+func (s *Storage) SaveConsumption(c *Consumption) error {
+	return s.driver.Save(&c).Error
+}
+
+func (s *Storage) RetrieveConsumptionByResource(resourceId uuid.UUID) ([]Consumption, error) {
+	var consumptions []Consumption
+	err := s.driver.Where("resource_id = ?", resourceId).Find(&consumptions).Error
+	return consumptions, err
+}
+
+func (s *Storage) DeleteConsumption(workerID string) error {
+	return s.driver.Where("worker_id = ?", workerID).Delete(&Consumption{}).Error
+}
